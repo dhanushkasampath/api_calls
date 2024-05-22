0f70377385fa8e764dda197457a6d8723c3d2a68
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-05-22 11:21:57 +0530

    added actuator dependency

--- src/main/java/com/example/api_calls/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/api_calls/advice/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.example.api_calls.exceptions;
+package com.example.api_calls.advice;
 
+import com.example.api_calls.exceptions.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
